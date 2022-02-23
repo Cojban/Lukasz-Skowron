7fa82224cc0320633c00a695b2e8d1c02a6eae22
Łukasz Skowron 3i2a <slukasz2003@gmail.com>
2022-02-23 17:00:11 +0100

    commit lukaszskowron

--- server01.js
+++ server01.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app!</h1>")
+    res.send('{imie: "Łukasz", nazwisko: "Skowron", klasa: "3I2", grupa: "3I2a"')
 })
 
 app.listen(PORT, function () {
